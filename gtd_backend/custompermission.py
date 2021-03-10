02fa3fbbfb171216170d8cf3d4a205cd583fecc5
lethanhtupk <lethanhtu1551998@gmail.com>
2021-03-10 22:55:36 +0700

    update custom permission classes

--- gtd_backend/custompermission.py
+++ gtd_backend/custompermission.py
@@ -9,10 +9,7 @@ class IsCurrentUserOwnerOrReadOnly(permissions.BasePermission):
 
 class IsAdminOrOwner(permissions.BasePermission):
   def has_object_permission(self, request, view, obj):
-    if request.method in permissions.SAFE_METHODS:
-      return True
-    else: 
-      return obj.user == request.user or obj.profile.role == 3 
+    return obj.owner == request.user or request.user.profile.role == 3 
 
 class IsAdmin(permissions.BasePermission):
   def has_permission(self, request, view):
