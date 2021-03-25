2f9560c49d83d915a55643b4ed061d477d1781bb
Tu Le Thanh <lethanhtu1551998@gmail.com>
2021-03-25 08:50:40 +0700

    custom reset password email

--- users/email.py
+++ users/email.py
@@ -1,5 +1,12 @@
-from djoser.email import ActivationEmail
+from djoser.email import (
+    ActivationEmail,
+    PasswordResetEmail
+)
 
 
 class CustomActivationEmail(ActivationEmail):
     template_name = 'email/activation.html'
+
+
+class CustomPasswordResetEmail(PasswordResetEmail):
+    template_name = 'email/password_reset.html'
