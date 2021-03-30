b6468b288524f3c31d26860ae1c44a56c82d6b74
Tu Le Thanh <lethanhtu1551998@gmail.com>
2021-03-30 11:53:57 +0700

    remove unused import

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,4 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
 from rest_framework.fields import SerializerMethodField
 from users.models import CustomUser, UserProfile
 
