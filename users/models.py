ce503821fb7f1026647c0485bb61b83828f77e16
Tu Le Thanh <lethanhtu1551998@gmail.com>
2021-03-31 14:57:41 +0700

    add request model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+from products.models import Seller
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext_lazy as _
@@ -29,11 +30,44 @@ class UserProfile(models.Model):
         (SELLER, 'Seller'),
         (ADMIN, 'Admin')
     )
+    seller = models.OneToOneField(
+        Seller,
+        on_delete=models.PROTECT,
+        related_name='owner',
+        null=True
+    )
     fullname = models.CharField(max_length=70)
     user = models.OneToOneField(
         CustomUser, on_delete=models.CASCADE, related_name='profile')
     photo_url = models.CharField(max_length=255, null=True)
     role = models.PositiveSmallIntegerField(ROLE_CHOICES, default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.user.fullname
+
+
+class Request(models.Model):
+    PENDING = 1
+    APPROVE = 2
+    REJECT = 3
+    STATUS_CHOICES = (
+        (PENDING, 'Pending'),
+        (APPROVE, 'Approve'),
+        (REJECT, 'Reject')
+    )
+    seller = models.ForeignKey(
+        Seller,
+        on_delete=models.CASCADE,
+        related_name='requests',
+    )
+    owner = models.OneToOneField(
+        UserProfile,
+        on_delete=models.CASCADE,
+        related_name='request',
+    )
+    reject_reason = models.TextField(default='')
+    status = models.PositiveSmallIntegerField(STATUS_CHOICES, default=1)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
