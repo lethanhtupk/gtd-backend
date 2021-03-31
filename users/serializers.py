a3605c223433c42c4a23b1d71177622881ce5b32
Tu Le Thanh <lethanhtu1551998@gmail.com>
2021-03-31 14:59:47 +0700

    serializer for request model

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from rest_framework.fields import SerializerMethodField
-from users.models import CustomUser, UserProfile
+from users.models import CustomUser, Request, UserProfile
+from products.serializers import SellerSerializer
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -14,10 +15,12 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
     email = SerializerMethodField()
     user = UserSerializer(read_only=True)
+    seller = SellerSerializer(read_only=True)
 
     class Meta:
         model = UserProfile
-        fields = ('id', 'email', 'fullname', 'photo_url', 'role', 'user')
+        fields = ('id', 'email', 'fullname',
+                  'photo_url', 'role', 'user', 'seller')
 
     def update(self, instance, validated_data):
         fullname = validated_data.get('fullname')
@@ -41,3 +44,32 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
     def get_email(self, obj):
         return CustomUser.objects.get(profile=obj).email
+
+
+class RequestCreateSerializer(serializers.ModelSerializer):
+    status = serializers.IntegerField(read_only=True)
+    owner = serializers.ReadOnlyField(source='owner.user.email')
+
+    class Meta:
+        model = Request
+        fields = ('seller', 'status', 'created_at', 'updated_at', 'owner')
+
+
+class RequestUpdateSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.user.email')
+
+    class Meta:
+        model = Request
+        fields = ('status', 'reject_reason', 'owner',
+                  'created_at', 'updated_at')
+
+    def validate(self, attrs):
+        status = attrs.get('status')
+        reject_reason = attrs.get('reject_reason')
+        if status == 2 and reject_reason:
+            raise serializers.ValidationError(
+                {'detail': 'Do not need reject reason for the request have been approve'})
+        if status == 3 and not reject_reason:
+            raise serializers.ValidationError(
+                {'detail': 'You need to provide reason for your rejection'})
+        return super().validate(attrs)
