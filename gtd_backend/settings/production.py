34328a13292db7fedc120332f7aceb5185ab757e
Tu Le Thanh <lethanhtu1551998@gmail.com>
2021-04-08 16:32:51 +0700

    update settings for heroku

--- gtd_backend/settings/production.py
+++ gtd_backend/settings/production.py
@@ -1,17 +1,13 @@
 from .base import *
-import os
+import dj_database_url
+from dj_database_url import config
 
 DEBUG = False
 
 ALLOWED_HOSTS = []
 
 DATABASES = {
-    'default': {
-        'ENGINE': os.environ.get('SQL_ENGINE', 'django.db.backends.sqlite3'),
-        'NAME': os.environ.get('SQL_DATABASE', os.path.join(BASE_DIR, 'db.sqlite3')),
-        'USER': os.environ.get('SQL_USER', 'user'),
-        'PASSWORD': os.environ.get('SQL_PASSWORD', 'password'),
-        'HOST': os.environ.get('SQL_HOST', 'localhost'),
-        'PORT': os.environ.get('SQL_PORT', ''),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
