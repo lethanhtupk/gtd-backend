7fed188ab3bfb6bfba1766b770058af4c2c19a0d
Tu Le Thanh <lethanhtu1551998@gmail.com>
2021-04-11 20:34:39 +0700

    config file aws

--- gtd_backend/settings/base.py
+++ gtd_backend/settings/base.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gtd-apt-test.eba-uq864qkq.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
